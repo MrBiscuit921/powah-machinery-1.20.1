032e9ccb245d77c349f665dc46365a0fb8289a34
MrBiscuit <122865263+MrBiscuit921@users.noreply.github.com>
2025-01-04 23:17:54 +1300

    Initial commit

---
+++ src/main/java/net/mrbiscuit/powahmachinery/PowahMachineryClient.java
@@ -0,0 +1,10 @@
+package net.mrbiscuit.powahmachinery;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PowahMachineryClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
